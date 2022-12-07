6808527d9ae804669905e134dbbad7ec210d05d7
schristinelee <christinelee1527@gmail.com>
2022-12-07 09:35:41 -0800

    js practices

---
+++ tasks.js
@@ -0,0 +1,28 @@
+// function firstTask() {
+//   console.log("first task done!");
+// }
+
+// function secondTask() {
+//   console.log("second task done!");
+// }
+
+// setTimeout(firstTask, 2000);
+// secondTask();
+
+// setTimeout(function () {
+//   console.log("first task done!");
+// }, 2000);
+
+// console.log("second task done!");
+
+setTimeout(function () {
+  console.log("First task done!");
+}, 2000);
+
+setTimeout(function () {
+  console.log("Second task done!");
+}, 4000);
+
+setTimeout(function () {
+  console.log("Third task done!");
+}, 6000);
---
+++ tasks.rb
@@ -0,0 +1,15 @@
+# def first_task
+#   puts "first task done!"
+# end
+
+# def second_task
+#   puts "second task done!"
+# end
+
+# sleep 2
+# first_task
+# second_task
+
+# sleep 2
+# puts "first task done!"
+# puts "second task done!"
